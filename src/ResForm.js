cf472caa2f3da7f3f8af29b1c4d501ea987893ae
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-01 11:09:10 -0600

    Add form guts

--- src/ResForm.js
+++ src/ResForm.js
@@ -11,10 +11,29 @@ class ResForm extends Component {
     }
   }
 
+  handelSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+
+  changeState = (name,value) => {
+    this.setState({
+      [name]:value
+    })
+  }
+
   render () {
+    const {name, date, time, number} = this.state
     return(
       <div>
-        ResForm
+        <form onSubmit={this.handelSubmit}>
+          <input name='name' onChange={this.changeState} value={name} type="text" />
+          <input name='date' onChange={this.changeState} value={date} type="text" />
+          <input name='time' onChange={this.changeState} value={time} type="text" />
+          <input name='number'onChange={this.changeState} value={number} type="text" />
+          <button>submit</button>
+        </form>
       </div>
     )
   }
